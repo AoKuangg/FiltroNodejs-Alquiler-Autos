9b913d85fae47364090c3ecdbf650ec9ab94511c
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-07-31 15:16:13 -0500

    feat: :sparkles: Se agrega siguiente endpoint el cual filtra a los empleados por cargo y este en especifico trae a los empleados con el cargo de vendedor

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import appClientes from "./routers/Clientes.js";
 import appAutos from "./routers/Autos.js";
 import appAlquiler from "./routers/Alquiler.js";
 import appReserva from "./routers/Reserva.js";
+import appEmpleados from "./routers/Empleados.js";
 dotenv.config();
 
 const AlquilerApp = express();
@@ -15,6 +16,7 @@ AlquilerApp.use("/clientes",appClientes);
 AlquilerApp.use("/autos",appAutos);
 AlquilerApp.use("/alquiler",appAlquiler);
 AlquilerApp.use("/reserva",appReserva);
+AlquilerApp.use("/empleados",appEmpleados);
 
 
 let config = JSON.parse(process.env.MY_CONFIG);
---
+++ routers/Empleados.js
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+const appEmpleados = Router();
+
+let con = undefined;
+appEmpleados.use((req,res,next)=>{
+   try {
+        let config_con = JSON.parse(process.env.CONECTION);
+        con = mysql.createPool(config_con);
+        next();
+   } catch (error) {
+        res.status(404).send("Couldn't connect to the database")
+   } 
+});
+
+appEmpleados.get("/",(req,res)=>{
+    con.query(
+        `SELECT * FROM Empleado WHERE Cargo = "Vendedor"
+        `, (error,results)=>{
+            if(error){
+                console.log(error);
+                res.status(500).send("Error executing query")
+            }else{
+                res.status(200).send(results);
+            }
+        }
+    )
+});
+
+
+
+
+
+
+
+export default appEmpleados;
