5682d344f0321da158c6fb0839911fd87d607ff0
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-07-31 15:08:39 -0500

    feat: :sparkles: Se agrega el siguiente endpoint relacionado con las reservas de los vehiculos, aparte se trae la informacion del cliente y de el automovil reservado

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from "express";
 import appClientes from "./routers/Clientes.js";
 import appAutos from "./routers/Autos.js";
 import appAlquiler from "./routers/Alquiler.js";
+import appReserva from "./routers/Reserva.js";
 dotenv.config();
 
 const AlquilerApp = express();
@@ -13,6 +14,7 @@ AlquilerApp.use(express.json());
 AlquilerApp.use("/clientes",appClientes);
 AlquilerApp.use("/autos",appAutos);
 AlquilerApp.use("/alquiler",appAlquiler);
+AlquilerApp.use("/reserva",appReserva);
 
 
 let config = JSON.parse(process.env.MY_CONFIG);
---
+++ routers/Reserva.js
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+const appReserva = Router();
+
+let con = undefined;
+appReserva.use((req,res,next)=>{
+   try {
+        let config_con = JSON.parse(process.env.CONECTION);
+        con = mysql.createPool(config_con);
+        next();
+   } catch (error) {
+        res.status(404).send("Couldn't connect to the database")
+   } 
+});
+
+appReserva.get("/",(req,res)=>{
+    con.query(
+        `SELECT r.ID_Reserva, r.ID_Cliente, r.ID_Automovil, r.Fecha_Reserva, r.Fecha_Inicio, r.Fecha_Fin, r.Estado, c.Nombre, c.Apellido, c.DNI, c.Direccion,
+        c.Telefono, c.Email, a.Marca, a.Modelo, a.Anio, a.Tipo, a.Capacidad, a.Precio_Diario
+        FROM Reserva r
+        INNER JOIN Cliente c ON r.ID_Cliente = c.ID_Cliente
+        INNER JOIN Automovil a ON r.ID_Automovil = a.ID_Automovil
+        WHERE r.Estado = "Pendiente"
+        `, (error,results)=>{
+            if(error){
+                console.log(error);
+                res.status(500).send("Error executing query")
+            }else{
+                res.status(200).send(results);
+            }
+        }
+    )
+});
+
+
+
+
+
+
+
+export default appReserva;
